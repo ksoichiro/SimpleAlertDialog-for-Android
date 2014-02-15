253d738ed0fd2d5d5c3f6c1172e91714511e7261
ksoichiro <soichiro.kashima@gmail.com>
2014-02-15 11:38:22 +0900

    Modified license comments and added LICENSE.txt.

--- samples/demos/src/com/simplealertdialog/sample/demos/AboutActivity.java
+++ samples/demos/src/com/simplealertdialog/sample/demos/AboutActivity.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 Soichiro Kashima
+ * Copyright 2014 Soichiro Kashima
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
