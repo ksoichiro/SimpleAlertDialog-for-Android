95637dab4e56fd48ac3d2867ba86ce4dc69e330f
ksoichiro <soichiro.kashima@gmail.com>
2014-03-09 04:40:17 +0900

    Added more tests.

--- tests/src/com/simplealertdialog/test/SimpleAlertDialogFragmentTest.java
+++ tests/src/com/simplealertdialog/test/SimpleAlertDialogFragmentTest.java
@@ -1,10 +1,10 @@
 package com.simplealertdialog.test;
 
-import com.simplealertdialog.SimpleAlertDialog;
-import com.simplealertdialog.SimpleAlertDialogFragment;
-
 import android.os.Bundle;
 import android.test.InstrumentationTestCase;
+
+import com.simplealertdialog.SimpleAlertDialog;
+import com.simplealertdialog.SimpleAlertDialogFragment;
 
 public class SimpleAlertDialogFragmentTest extends InstrumentationTestCase {
 
@@ -22,10 +22,20 @@ public class SimpleAlertDialogFragmentTest extends InstrumentationTestCase {
         SimpleAlertDialogFragment.Builder builder = new SimpleAlertDialogFragment.Builder();
         Bundle args = builder.createArguments();
         assertNotNull(args);
-        assertEquals(args.getString(SimpleAlertDialog.ARG_TITLE_RES_ID), null);
+        assertEquals(args.getInt(SimpleAlertDialog.ARG_TITLE_RES_ID, -1), -1);
         assertEquals(builder.setTitle(R.string.title), builder);
         args = builder.createArguments();
-        assertEquals(args.getInt(SimpleAlertDialog.ARG_TITLE_RES_ID), R.string.title);
+        assertEquals(args.getInt(SimpleAlertDialog.ARG_TITLE_RES_ID, -1), R.string.title);
+    }
+
+    public void testBuilderSetIcon() {
+        SimpleAlertDialogFragment.Builder builder = new SimpleAlertDialogFragment.Builder();
+        Bundle args = builder.createArguments();
+        assertNotNull(args);
+        assertEquals(args.getInt(SimpleAlertDialog.ARG_ICON, -1), -1);
+        assertEquals(builder.setIcon(android.R.drawable.ic_dialog_alert), builder);
+        args = builder.createArguments();
+        assertEquals(args.getInt(SimpleAlertDialog.ARG_ICON, -1), android.R.drawable.ic_dialog_alert);
     }
 
     public void testBuilderSetMessage() {
@@ -42,10 +52,56 @@ public class SimpleAlertDialogFragmentTest extends InstrumentationTestCase {
         SimpleAlertDialogFragment.Builder builder = new SimpleAlertDialogFragment.Builder();
         Bundle args = builder.createArguments();
         assertNotNull(args);
-        assertEquals(args.getString(SimpleAlertDialog.ARG_MESSAGE_RES_ID), null);
+        assertEquals(args.getInt(SimpleAlertDialog.ARG_MESSAGE_RES_ID, -1), -1);
         assertEquals(builder.setMessage(R.string.message), builder);
         args = builder.createArguments();
-        assertEquals(args.getInt(SimpleAlertDialog.ARG_MESSAGE_RES_ID), R.string.message);
+        assertEquals(args.getInt(SimpleAlertDialog.ARG_MESSAGE_RES_ID, -1), R.string.message);
+    }
+
+    public void testBuilderSetPositiveButton() {
+        SimpleAlertDialogFragment.Builder builder = new SimpleAlertDialogFragment.Builder();
+        Bundle args = builder.createArguments();
+        assertNotNull(args);
+        assertEquals(args.getString(SimpleAlertDialog.ARG_POSITIVE_BUTTON), null);
+        assertEquals(builder.setPositiveButton("POSITIVE"), builder);
+        args = builder.createArguments();
+        assertEquals(args.getString(SimpleAlertDialog.ARG_POSITIVE_BUTTON), "POSITIVE");
+    }
+
+    public void testBuilderSetPositiveButtonByResources() {
+        SimpleAlertDialogFragment.Builder builder = new SimpleAlertDialogFragment.Builder();
+        Bundle args = builder.createArguments();
+        assertNotNull(args);
+        assertEquals(args.getInt(SimpleAlertDialog.ARG_POSITIVE_BUTTON_RES_ID, -1), -1);
+        assertEquals(builder.setPositiveButton(android.R.string.ok), builder);
+        args = builder.createArguments();
+        assertEquals(args.getInt(SimpleAlertDialog.ARG_POSITIVE_BUTTON_RES_ID, -1), android.R.string.ok);
+    }
+
+    public void testBuilderSetNegativeButton() {
+        SimpleAlertDialogFragment.Builder builder = new SimpleAlertDialogFragment.Builder();
+        Bundle args = builder.createArguments();
+        assertNotNull(args);
+        assertEquals(args.getString(SimpleAlertDialog.ARG_NEGATIVE_BUTTON), null);
+        assertEquals(builder.setNegativeButton("NEGATIVE"), builder);
+        args = builder.createArguments();
+        assertEquals(args.getString(SimpleAlertDialog.ARG_NEGATIVE_BUTTON), "NEGATIVE");
+    }
+
+    public void testBuilderSetNegativeButtonByResources() {
+        SimpleAlertDialogFragment.Builder builder = new SimpleAlertDialogFragment.Builder();
+        Bundle args = builder.createArguments();
+        assertNotNull(args);
+        assertEquals(args.getInt(SimpleAlertDialog.ARG_NEGATIVE_BUTTON_RES_ID, -1), -1);
+        assertEquals(builder.setNegativeButton(android.R.string.cancel), builder);
+        args = builder.createArguments();
+        assertEquals(args.getInt(SimpleAlertDialog.ARG_NEGATIVE_BUTTON_RES_ID, -1), android.R.string.cancel);
+    }
+
+    public void testBuilderCreate() {
+        SimpleAlertDialogFragment.Builder builder = new SimpleAlertDialogFragment.Builder();
+        SimpleAlertDialogFragment fragment = builder.create();
+        assertNotNull(fragment);
     }
 
 }
