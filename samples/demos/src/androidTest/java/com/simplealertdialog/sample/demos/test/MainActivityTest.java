baf7e4558f7bb208fd732eefc1f7405f355d5797
ksoichiro <soichiro.kashima@gmail.com>
2014-05-11 16:52:29 +0900

    Added JaCoCo settings to demo app (but disabled) and added some test cases.

--- samples/demos/src/androidTest/java/com/simplealertdialog/sample/demos/test/MainActivityTest.java
+++ samples/demos/src/androidTest/java/com/simplealertdialog/sample/demos/test/MainActivityTest.java
@@ -1,15 +1,43 @@
 package com.simplealertdialog.sample.demos.test;
 
+import android.app.Instrumentation;
 import android.test.InstrumentationTestCase;
+import android.view.KeyEvent;
 
+import com.simplealertdialog.sample.demos.AboutActivity;
 import com.simplealertdialog.sample.demos.MainActivity;
 
 public class MainActivityTest extends InstrumentationTestCase {
 
+    private MainActivity mainActivity;
+
     public void testLaunch() {
-        MainActivity activity = launchActivity("com.simplealertdialog.sample.demos", MainActivity.class, null);
+        mainActivity = launchActivity("com.simplealertdialog.sample.demos", MainActivity.class, null);
         getInstrumentation().waitForIdleSync();
-        assertNotNull(activity);
+        assertNotNull(mainActivity);
+        mainActivity.finish();
     }
 
+    public void testLaunchAboutActivity() {
+        Instrumentation.ActivityMonitor monitorAbout = new Instrumentation.ActivityMonitor(AboutActivity.class.getCanonicalName(), null, false);
+        getInstrumentation().addMonitor(monitorAbout);
+
+        mainActivity = launchActivity("com.simplealertdialog.sample.demos", MainActivity.class, null);
+        getInstrumentation().waitForIdleSync();
+
+        mainActivity.openOptionsMenu();
+        getInstrumentation().waitForIdleSync();
+
+        getInstrumentation().sendKeyDownUpSync(KeyEvent.KEYCODE_DPAD_DOWN);
+        getInstrumentation().waitForIdleSync();
+
+        getInstrumentation().sendKeyDownUpSync(KeyEvent.KEYCODE_ENTER);
+        getInstrumentation().waitForIdleSync();
+
+        AboutActivity aboutActivity = (AboutActivity) getInstrumentation().waitForMonitorWithTimeout(monitorAbout, 3000);
+        assertNotNull(aboutActivity);
+        assertTrue(getInstrumentation().checkMonitorHit(monitorAbout, 1));
+
+        aboutActivity.finish();
+    }
 }
