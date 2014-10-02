320df80574d1e786fc4360c09a32c545b230acbf
ksoichiro <soichiro.kashima@gmail.com>
2014-10-03 00:33:06 +0900

    Added a test for setUseAdapter.

--- simplealertdialog-tests/src/com/simplealertdialog/SimpleAlertDialogFragmentTest.java
+++ simplealertdialog-tests/src/com/simplealertdialog/SimpleAlertDialogFragmentTest.java
@@ -145,6 +145,16 @@ public class SimpleAlertDialogFragmentTest extends InstrumentationTestCase {
         assertTrue(args.getBoolean(SimpleAlertDialog.ARG_USE_VIEW, false));
     }
 
+    public void testBuilderSetUseAdapter() {
+        SimpleAlertDialogFragment.Builder builder = new SimpleAlertDialogFragment.Builder();
+        Bundle args = builder.createArguments();
+        assertNotNull(args);
+        assertFalse(args.getBoolean(SimpleAlertDialog.ARG_USE_ADAPTER, true));
+        builder.setUseAdapter(true);
+        args = builder.createArguments();
+        assertTrue(args.getBoolean(SimpleAlertDialog.ARG_USE_ADAPTER, false));
+    }
+
     public void testBuilderSetNegativeButtonByResources() {
         SimpleAlertDialogFragment.Builder builder = new SimpleAlertDialogFragment.Builder();
         Bundle args = builder.createArguments();
