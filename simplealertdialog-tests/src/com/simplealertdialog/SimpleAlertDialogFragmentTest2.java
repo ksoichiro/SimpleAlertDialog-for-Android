96fdda028d99e28825616bb0c18259c2a235510e
ksoichiro <soichiro.kashima@gmail.com>
2014-10-03 02:58:37 +0900

    Added more tests using Activity.

--- simplealertdialog-tests/src/com/simplealertdialog/SimpleAlertDialogFragmentTest2.java
+++ simplealertdialog-tests/src/com/simplealertdialog/SimpleAlertDialogFragmentTest2.java
@@ -28,8 +28,23 @@ public class SimpleAlertDialogFragmentTest2 extends ActivityInstrumentationTestC
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     public void testBuilderCreateHasParams() {
         new SimpleAlertDialogFragment.Builder()
+                .setIcon(android.R.drawable.ic_dialog_alert)
                 .setTitle("Test")
                 .setMessage("Test")
+                .setPositiveButton("OK")
+                .setNegativeButton("Cancel")
+                .create().show(getActivity().getFragmentManager(), "dialog");
+        getInstrumentation().waitForIdleSync();
+        Fragment f = getActivity().getFragmentManager().findFragmentByTag("dialog");
+        assertNotNull(f);
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public void testBuilderCreateHasParamsNoIcon() {
+        new SimpleAlertDialogFragment.Builder()
+                .setTitle("Test")
+                .setMessage("Test")
+                .setPositiveButton("OK")
                 .create().show(getActivity().getFragmentManager(), "dialog");
         getInstrumentation().waitForIdleSync();
         Fragment f = getActivity().getFragmentManager().findFragmentByTag("dialog");
